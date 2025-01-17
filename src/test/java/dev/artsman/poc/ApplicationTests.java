f9acc7944679c0258096752236450931df93670f
Bruno Andrade <bruno@artsman.dev>
2025-01-17 12:57:59 +0000

    (feat): base.

---
+++ src/test/java/dev/artsman/poc/ApplicationTests.java
@@ -0,0 +1,19 @@
+package dev.artsman.poc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class ApplicationTests {
+	@Autowired
+	ApplicationContext context;
+
+	@Test
+	void contextLoads() {
+		var application = context.getBean(Application.class);
+		Assertions.assertNotNull(application);
+	}
+}

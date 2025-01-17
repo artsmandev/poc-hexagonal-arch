1cee64b6a0188271bfe324d2a10039d7701af038
Bruno Andrade <bruno@artsman.dev>
2025-01-17 13:53:11 +0000

    (refact): replaced record by class.

--- src/main/java/dev/artsman/poc/hexagonal/application/core/domain/Customer.java
+++ src/main/java/dev/artsman/poc/hexagonal/application/core/domain/Customer.java
@@ -1,5 +1,78 @@
 package dev.artsman.poc.hexagonal.application.core.domain;
 
+import java.util.Objects;
 import java.util.UUID;
 
-public record Customer(UUID id, String name, Address address, String cpf, boolean isValid) {}
+public class Customer {
+	private UUID id;
+	private String name;
+	private Address address;
+	private String cpf;
+	private boolean isValid;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public boolean isValid() {
+		return isValid;
+	}
+
+	public void setValid(boolean valid) {
+		isValid = valid;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Customer customer)) {
+			return false;
+		}
+		return Objects.equals(id, customer.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
+
+	@Override
+	public String toString() {
+		return "Customer[" +
+			"id=" + id + ", " +
+			"name=" + name + ", " +
+			"address=" + address + ", " +
+			"cpf=" + cpf + ", " +
+			"isValid=" + isValid + ']';
+	}
+}
